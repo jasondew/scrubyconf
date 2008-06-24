b388c35407a3ddb88d37af88be0a4a3a15b0f9d9
Jason Dew <jason.dew@gmail.com>
2008-06-24 16:23:17 -0400

    updated layout, added ruby image

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
+  map.root :controller => 'pages', :action => 'home'
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'

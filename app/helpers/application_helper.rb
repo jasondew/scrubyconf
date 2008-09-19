7899bb0584e2123096b5c7006548e5bda6aa784c
Jason Dew <jason.dew@gmail.com>
2008-09-19 16:27:59 -0400

    added agenda and some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
   def label *args, &block
-    label_text, content, *options = args
+    label_text, content, options = args
+    (options ||= Hash.new).merge! :class => "form"
     content = capture(&block) if block_given?
 
-    content_tag(:dl, *options) do
+    content_tag(:dl, options) do
       content_tag(:dt, content_tag(:label, "#{label_text}: ")) + content_tag(:dd, content.to_s || "")
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :proposals, :collection => { :list => :get }
 
   map.sponsorship_levels '/sponsorship_levels', :controller => 'pages', :action => 'sponsorship_levels'
+  map.agenda '/agenda', :controller => 'pages', :action => 'agenda'
 
   map.root :controller => 'pages', :action => 'home'
 

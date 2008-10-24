352d7b8c16dc5d6432586e2e9c34644f49b4a2e0
Jason Dew <jason.dew@gmail.com>
2008-10-24 15:14:16 -0400

    andrews changes from the server and presentations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.sponsorship_levels '/sponsorship_levels', :controller => 'pages', :action => 'sponsorship_levels'
   map.agenda '/agenda', :controller => 'pages', :action => 'agenda'
+  map.benchmark '/benchmark_consulting', :controller => 'pages', :action => 'bc'
+  map.engineyard '/engine_yard', :controller => 'pages', :action => 'ey'
 
   map.root :controller => 'pages', :action => 'home'
 
